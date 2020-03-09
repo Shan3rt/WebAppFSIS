323c069a48f12a492fa4f2c57b5dc152e958805f
Shan3rt <54223412+Shan3rt@users.noreply.github.com>
2020-03-09 16:12:51 -0600

    b

---
+++ FSISSystem/BLL/GuardianController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FSISSystem.DAL;
+using FSISSystem.ENTITIES;
+
+namespace FSISSystem.BLL
+{
+    class GuardianController
+    {
+        public Guardian Guardians_FindByID(int guardianid)
+        {
+            using (var context = new FSISContext())
+            {
+                return context.Guardians.Find(guardianid);
+            }
+        }
+    }
+}
---
+++ FSISSystem/ENTITIES/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSISSystem.ENTITIES
+{
+    // [Table("Region")] //annotation. map c# class to actual table
+    class Team
+    {
+        //[Key]//Used if "Id"/"ID" is not used at the end (assumed that those already are), otherwise not.
+    }
+}
+//property, auto implemented (get set - no code)
